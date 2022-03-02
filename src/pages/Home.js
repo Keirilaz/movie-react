cede7e1b5a4e2265a53a2222146979b314a41b85
Kévin <kevinvendome@hotmail.com>
2022-03-02 13:43:35 +0100

    message

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Form from "../components/Form";
+import Header from "../components/Header";
+
+function Home() {
+    return (
+        <div className="home-page">
+            <Header />
+            <Form />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/UserList.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
+
+
+function UserList() {
+    const [listData, setListData] = useState([]);
+
+    useEffect(() => {
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(",") : [];
+
+        for (let i = 0; i < moviesId.length; i++) {
+
+            axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`)
+                .then((res) => setListData((listData) => [...listData, res.data]))
+        }
+    }, [])
+
+    return (
+        <div className="user-list-page" >
+            <Header />
+            <h2>Coup de coeur❤️</h2>
+            <div className="result">
+                {listData.length > 0 ? listData.map((movie) => (<Card movie={movie} key={movie.id} />)) : <h2>Aucun coup de coeur pour le moment</h2>}
+            </div>
+        </div>
+    );
+};
+
+export default UserList;
